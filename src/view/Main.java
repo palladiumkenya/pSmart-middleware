446ac5d5bdf1c240ab93bdddd1978950a186d1ba
ojwanganto <ojwangwachiaje@gmail.com>
2018-02-07 18:14:03 +0300

    added design for login page

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,10 @@
-package sample;
+package view;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -26,7 +27,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        BorderPane border = new BorderPane();
+        /*BorderPane border = new BorderPane();
         HBox hbox = addApplicationHeader();
         HBox applicationStatus = addApplicationStatusHeader();
 
@@ -35,12 +36,20 @@ public class Main extends Application {
         //border.setTop(applicationStatus);
 
         border.setLeft(addActionPane());
-        addStackPane(hbox);         // Add stack to HBox in top region
+        addStackPane(hbox);    */     // Add stack to HBox in top region
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("P-SMART");
+        Scene loginScene = new Scene(root);
+        primaryStage.setScene(loginScene);
+        primaryStage.setResizable(false);
 
-        Scene scene = new Scene(border);
+
+
+
+        /*Scene scene = new Scene(border);
         primaryStage.setScene(scene);
         primaryStage.setTitle("P-SMART");
-        primaryStage.setMaximized(true);
+        primaryStage.setMaximized(true);*/
         primaryStage.show();
 
     }
--- src/view/SampleController.java
+++ src/view/SampleController.java
@@ -1,4 +1,4 @@
-package sample;
+package view;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
